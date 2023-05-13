84ce3155e7d8e1f5c9bddddfca7c5b9819b3c1a2
Faridmech <feridh189@gmail.com>
2023-05-13 19:59:34 +0400

    Finish

--- copy.js
+++ copy.js
@@ -1,107 +1,96 @@
-const allTabsBody = document.querySelectorAll(".tab-body-single");
-const allTabsHead = document.querySelectorAll(".tab-head-single");
-const searchForum = document.querySelector(".app-header-search")
-let searchList = document.getElementById("search-list")
+const allTabsBody = document.querySelectorAll('.tab-body-single');
+const allTabsHead = document.querySelectorAll('.tab-head-single');
+const searchForm = document.querySelector('.app-header-search');
+let searchList = document.getElementById('search-list');
 
 let activeTab = 1, allData;
-const init = ()=>{
-    showActiveTabsBody();
-    showActiveTabsHead();
+
+const init = () => {
+    showActiveTabBody();
+    showActiveTabHead();
 }
 
-const showActiveTabsHead = () => allTabsHead[activeTab - 1].classList.add("active-tab");
+const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab');
 
-const showActiveTabsBody = () =>{
+const showActiveTabBody = () => {
     hideAllTabBody();
-    allTabsBody[activeTab -1].classList.add("show-tab")
+    allTabsBody[activeTab - 1].classList.add('show-tab');
 }
 
-const hideAllTabBody = () =>{
-    allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove("show-tab"))
-}
-const hideAllTabHead = () =>{
-    allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove("active-tab"))
-}
+const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'));
+const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'));
 
-
-window.addEventListener("DOMContentLoaded", ()=> init());
-
-allTabsHead.forEach(singleTabHead => 
-    singleTabHead.addEventListener("click" , () =>{
+// even listeners
+window.addEventListener('DOMContentLoaded', () => init());
+// button event listeners
+allTabsHead.forEach(singleTabHead => {
+    singleTabHead.addEventListener('click', () => {
         hideAllTabHead();
         activeTab = singleTabHead.dataset.id;
-        showActiveTabsHead();
-        showActiveTabsBody();
-    }))
-
-    
-    const getInputValue = (event) =>{
-        event.preventDefault();
-        let searchText = searchForum.search.value;
-        fetchAllSuperHero(searchText);
-    }
-    
-    searchForum.addEventListener("submit", getInputValue);
-
-
-
-    // api key 727054372039115
-
-    const fetchAllSuperHero = async(searchText)=>{
-
-        let url = `https://www.superheroapi.com/api.php/727054372039115/search/${searchText}`;
-    
-        try{
-            const response = await fetch(url);
-            allData = await response.json();
-            if(allData.response === 'success'){
-                // console.log(allData);
-                showSearchList(allData.results);
-            }
-        } catch(error){
-            console.log(error);
-        }
-                     
-
-    }
-     
-
-    const showSearchList =(data)=>{
-        searchList.innerHTML ="";
-        data.forEach(dataItem=>{
-            const divelement = document.createElement("div");
-            divelement.classList.add("search-list-item");
-            divelement.innerHTML=`
-                    <img src="${dataItem.image.url ? dataItem.image.url:""}">
-                    <p data-id ="${dataItem.id}">${dataItem.name}</p>
-            `;
-        searchList.appendChild(divelement);
-        })
-    }
-
-    searchForum.search.addEventListener("keyup", ()=>{
-        if(searchForum.search.value.length >1){
-            fetchAllSuperHero(searchForum.search.value)
-        }else{
-            searchList.innerHTML ="";
-        }
-    })
-    
-    searchList.addEventListener('click', (event) => {
-        let searchId = event.target.dataset.id;
-        let singleData = allData.results.filter(singleData => {
-            return searchId === singleData.id;
-        })
-        showSuperheroDetails(singleData);
-        searchList.innerHTML = "";
+        showActiveTabHead();
+        showActiveTabBody();
     });
+});
 
-    const showSuperheroDetails = (data) =>{
-        
-        document.querySelector('.app-body-content-thumbnail').innerHTML = `
+const getInputValue = (event) => {
+    event.preventDefault();
+    let searchText = searchForm.search.value;
+    fetchAllSuperHero(searchText);
+}
+
+// search form submission
+searchForm.addEventListener('submit', getInputValue);
+
+// api key => 727054372039115
+const fetchAllSuperHero = async(searchText) => {
+    let url = `https://www.superheroapi.com/api.php/727054372039115/search/${searchText}`;
+    try{
+        const response = await fetch(url);
+        allData = await response.json();
+        if(allData.response === 'success'){
+            // console.log(allData);
+            showSearchList(allData.results);
+        }
+    } catch(error){
+        console.log(error);
+    }
+}
+
+const showSearchList = (data) => {
+    searchList.innerHTML = "";
+    data.forEach(dataItem => {
+        const divElem = document.createElement('div');
+        divElem.classList.add('search-list-item');
+        divElem.innerHTML = `
+            <img src = "${dataItem.image.url ? dataItem.image.url : ""}" alt = "">
+            <p data-id = "${dataItem.id}">${dataItem.name}</p>
+        `;
+        searchList.appendChild(divElem);
+    });
+}
+
+searchForm.search.addEventListener('keyup', () => {
+    if(searchForm.search.value.length > 1){
+        fetchAllSuperHero(searchForm.search.value);
+    } else {
+        searchList.innerHTML = "";
+    }
+});
+
+searchList.addEventListener('click', (event) => {
+    let searchId = event.target.dataset.id;
+    let singleData = allData.results.filter(singleData => {
+        return searchId === singleData.id;
+    })
+    showSuperheroDetails(singleData);
+    searchList.innerHTML = "";
+});
+
+const showSuperheroDetails = (data) => {
+    console.log(data);
+    document.querySelector('.app-body-content-thumbnail').innerHTML = `
         <img src = "${data[0].image.url}">
     `;
-
 
     document.querySelector('.name').textContent = data[0].name;
     document.querySelector('.powerstats').innerHTML = `
@@ -225,4 +214,4 @@ allTabsHead.forEach(singleTabHead =>
         <span>${data[0].connections['relatives']}</span>
     </li>
     `;
-    }
+}
